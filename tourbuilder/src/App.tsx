1988a42b392ebb235d63e330c16a6074e8231730
Nick Clifford <nick@nickclifford.me>
2022-02-01 19:26:26 -0600

    styles

--- tourbuilder/src/App.tsx
+++ tourbuilder/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { ReactComponent as Logo } from "./logo.svg";
 import { ReactComponent as Map } from "./map.svg";
 import { getData, StateData } from "./backend";
+import "./App.scss";
 
 enum ViewInterval {
-	LOW = "0-250",
-	MEDIUM = "250-500",
-	HIGH = "500-1000",
+	LOW = "0 - 250",
+	MEDIUM = "250 - 500",
+	HIGH = "500 - 1000",
 	HIGHEST = "1000+",
 }
 
@@ -38,19 +40,26 @@ function App() {
 		.join(",");
 
 	return (
-		<div>
-			<h1>TourBuilder</h1>
-			<select
-				name="interval"
-				onChange={e => setInterval(e.target.value as ViewInterval)}
-			>
-				{Object.values(ViewInterval).map(i => (
-					<option value={i}>{i}</option>
-				))}
-			</select>
-			<style>{`${ids} { fill: #0D7EF9; }`}</style>
-			<Map />
-		</div>
+		<>
+			<Logo className="logo" />
+			<div className="visits-view">
+				<div className="container">
+					<div className="range-selector">
+						<h1><label htmlFor="interval">User visits</label></h1>
+						<select
+							id="interval"
+							onChange={e => setInterval(e.target.value as ViewInterval)}
+						>
+							{Object.values(ViewInterval).map(i => (
+								<option value={i}>{i}</option>
+							))}
+						</select>
+					</div>
+					<style>{`${ids} { fill: #0D7EF9; }`}</style>
+					<Map className="map" />
+				</div>
+			</div>
+		</>
 	);
 }
 
